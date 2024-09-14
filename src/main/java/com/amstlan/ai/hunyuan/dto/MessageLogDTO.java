459ae084c577df56edfcf31f6afb4a6714403bfd
lanzhs <ffos.lanzhs@chinatelecom.cn>
2024-09-14 17:03:00 +0800

    [fix]    添加部分注释

--- src/main/java/com/amstlan/ai/hunyuan/dto/MessageLogDTO.java
+++ src/main/java/com/amstlan/ai/hunyuan/dto/MessageLogDTO.java
@@ -1,18 +1,27 @@
 package com.amstlan.ai.hunyuan.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value = "MessageLogDTO", description = "消息日志表DTO")
 public class MessageLogDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "聊天的角色")
     private String role;
+
+    @ApiModelProperty(value = "聊天的内容")
     private String content;
+
+    @ApiModelProperty(value = "唯一标识")
     private String UUID;
 
 }
